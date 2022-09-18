43bbc88532ef0bd9a700edfbfbf98baf9ac64a76
Ксения <guzgks@gmail.com>
2022-09-19 01:15:53 +0300

    commit

--- src/test/java/com/demoqa/PracticeForm.java
+++ src/test/java/com/demoqa/PracticeForm.java
@@ -1,12 +1,15 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import javax.swing.plaf.basic.BasicBorders;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -22,15 +25,24 @@ public class PracticeForm {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Bananov");
-        $("[id=lastName]").setValue("Norman");
-        $("[id=userEmail]").setValue("banorman@demoqa.com");
+        $("#firstName").setValue("Bananov");
+        $("#lastName").setValue("Norman");
+        $("#userEmail").setValue("banorman@demoqa.com");
         $("#gender-radio-1").doubleClick();
-        $("[id=userNumber]").setValue("8909909909");
-        $("[id=dateOfBirth]").doubleClick();
-        $("[id=dateOfBirthInput]").click();
+        $("#userNumber").setValue("8909909909");
+        //$("#dateOfBirth").doubleClick(); вроде это не надо
+        $("#dateOfBirthInput").click();
         $("[class=react-datepicker__month-select]").click();
         $("[class=react-datepicker__month-select]").selectOption("April");
-
+        $("[class=react-datepicker__month-select]").click();
+        $("[class=react-datepicker__year-select]").click();
+        $("[class=react-datepicker__year-select]").selectOption("1997");
+        //$("#dateOfBirthInput").pressEnter();
+        //$("[class=react-datepicker__day react-datepicker__day--021]").click();
+        $("#subjectsInput").setValue("History").pressEnter();
+       // $("#hobbies-checkbox-2").click();
+        //$("#uploadPicture").doubleClick();
+        $("#uploadPicture").uploadFromClasspath("1.PNG");
+        //uploadPicture
     }
 }
